866354325e1a2dbc7d9f76b8f393d60076c54fbd
igrossberg23 <igrossberg23@gmail.com>
2022-05-11 10:27:25 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const heroHeading = document.querySelector('.hero-heading');
+const heroImage = document.querySelector('.hero-image');
+
+heroHeading.classList.add('text-slide-in');
+heroImage.classList.add('hero-image-slide-left');
